af2ef6563f8014f375c7823d7e1e29b942a1974c
Tharindu Adikari <5517829+thadikari@users.noreply.github.com>
2019-02-06 00:04:24 -0500

    impl llr based bsc SPA

--- spa.py
+++ spa.py
@@ -12,24 +12,6 @@ class SPA:
         self.var_neigh = tuple(np.transpose(np.nonzero(self.parity_mtx[:, ind])).flatten() for ind in range(n))
         self.chk_neigh = tuple(np.transpose(np.nonzero(self.parity_mtx[ind, :])).flatten() for ind in range(c))
 
-        self.chk_combs = []
-        for chk_ind in range(c):
-            degree = self.chk_neigh[chk_ind].shape[0]
-            power = 2 ** np.arange(degree)
-            d = np.arange(2 ** degree)
-            permutations = np.floor((d[:, None] % (2 * power)) / power).astype(int)
-            combs = permutations[permutations.sum(axis=1) % 2 == 0]
-            num_combs = combs.shape[0]
-
-            # number of terms in marginalization for var = 0 OR 1
-            # half of terms denote var = 0 and other half var = 1
-            num_terms = num_combs >> 1  # divide by 2
-            # which combinations correspond to var_i = 0, i in 0th dim
-            combs_whr_0 = np.where(combs.T == 0)[1].reshape([-1, num_terms])
-            # which combinations correspond to var_i = 1, i in 0th dim
-            combs_whr_1 = np.where(combs.T == 1)[1].reshape([-1, num_terms])
-            self.chk_combs.append((combs, combs_whr_0, combs_whr_1))
-
     def decode(self, y, priors):
 
         [c, n] = self.parity_mtx.shape
@@ -48,28 +30,22 @@ class SPA:
             for chk_ind in range(c):
                 # indices of connected variables
                 var_indices = self.chk_neigh[chk_ind]
-                degree = var_indices.shape[0]
-
                 chk_msg_in = var_to_chk[chk_ind, var_indices]
-                chk_in_exp = np.exp(chk_msg_in)
-                log_prob_0 = chk_in_exp / (1 + chk_in_exp)
-                log_prob_1 = 1 / (1 + chk_in_exp)
-                aug_chk_msg_in = np.vstack((log_prob_0, log_prob_1)).T
 
-                # pre computed valid var combinations
-                combs, combs_whr_0, combs_whr_1 = self.chk_combs[chk_ind]
-                # each row --> probs according to each combination
-                # includes the prob of receiving variable as well
-                # need to be subtracted next
-                prob_combs = aug_chk_msg_in[np.arange(degree), combs]
-                prod = prob_combs.prod(axis=1)  # multiply probs
+                if 0:  # min sum
+                    tiled = np.tile(chk_msg_in, (chk_msg_in.shape[0], 1))
+                    np.fill_diagonal(tiled, 1e5)
+                    sign = np.sign(tiled).prod(axis=1)
+                    np.fill_diagonal(tiled, np.Inf)
+                    min = np.min(np.abs(tiled), axis=1)
+                    chk_msg_out = sign * min
+                else:  # sum prod
+                    tiled = np.tile(chk_msg_in / 2., (chk_msg_in.shape[0], 1))
+                    tanned = np.tanh(tiled)
+                    np.fill_diagonal(tanned, 1.)
+                    prod = tanned.prod(axis=1)
+                    chk_msg_out = 2 * np.arctanh(prod)
 
-                # removing the contribution from the receiving variable
-                # sum_terms_0: terms to be summed in numerator
-                sum_terms_0 = prod[combs_whr_0] / log_prob_0[:, None]
-                sum_terms_1 = prod[combs_whr_1] / log_prob_1[:, None]
-
-                chk_msg_out = np.log(sum_terms_0.sum(axis=1)) - np.log(sum_terms_1.sum(axis=1))
                 chk_to_var[chk_ind, var_indices] = chk_msg_out
 
             # check if a codeword
@@ -82,6 +58,7 @@ class SPA:
             for var_ind in range(n):
                 chk_indices = self.var_neigh[var_ind]
                 var_msg_in = chk_to_var[chk_indices, var_ind]
-                prod = var_msg_in.sum() + priors[var_ind]
-                var_msg_out = prod - var_msg_in
+                tiled = np.tile(var_msg_in, (var_msg_in.shape[0], 1))
+                np.fill_diagonal(tiled, 0.)
+                var_msg_out = tiled.sum(axis=1) + priors[var_ind]
                 var_to_chk[chk_indices, var_ind] = var_msg_out
