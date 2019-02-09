2aee23473839df2df7d0c77d9e30c452d2e36963
Tharindu Adikari <5517829+thadikari@users.noreply.github.com>
2019-02-09 12:42:16 -0500

    impl spa for bec

--- bec.py
+++ bec.py
@@ -1,6 +1,6 @@
 import numpy as np
 import utils
-import spa
+import numpy.ma as ma
 
 
 class Channel:
@@ -31,11 +31,46 @@ class ML:
 
 class SPA:
     def __init__(self, p, code):
-        self.spa = spa.SPA(code.parity_mtx)
-        self.llr = np.array([np.inf, -np.inf, 0])
+        self.max_iter = 100
+        self.symbols = np.array([-1, 1, 0])
+        self.parity_mtx = ma.masked_array(code.parity_mtx.astype(int),
+                                          mask=~code.parity_mtx.astype(bool))
+        self.xx, self.yy = np.where(self.parity_mtx)
+        self.xy = (self.xx, self.yy)
 
     def decode(self, y):
-        return self.spa.decode(y, self.llr[y])
+        xx, yy, xy = self.xx, self.yy, self.xy
+        priors = self.symbols[y]
+        chk_to_var = self.parity_mtx + 0
+        var_to_chk = self.parity_mtx + 0
+        var_to_chk[xy] = priors[yy]
+
+        iter_count = 0
+        x_hat = y
+
+        while 1:
+            iter_count += 1
+            if iter_count > self.max_iter:
+                # print('max reached', y.sum(), x_hat.sum())
+                return x_hat
+
+            # chk_to_var
+            sums = (self.parity_mtx - np.abs(var_to_chk)).sum(axis=1)
+            chk_to_var.data[sums > 1, :] = 0  # more than 1 erasures
+            ind1 = sums == 1
+            sums1 = np.abs(var_to_chk[ind1, :])
+            sums2 = 2 * ((var_to_chk[ind1, :] > 0).astype(int).sum(axis=1) % 2) - 1
+            chk_to_var.data[ind1, :] = (1 - sums1) * sums2[:, None]
+            chk_to_var.data[sums == 0, :] = var_to_chk.data[sums == 0, :]
+
+            # check if no erasures anymore
+            marginal = np.sign(priors + chk_to_var.sum(axis=0))
+            x_hat = np.array([2, 1, 0])[marginal]
+            if np.sum(x_hat == 2) == 0: return x_hat
+
+            # var_to_chk
+            var_in_sum = chk_to_var.sum(axis=0) + priors
+            var_to_chk.data[xy] = np.sign(var_in_sum[yy] - chk_to_var[xy])
 
 
 class Test(utils.TestCase):
@@ -46,7 +81,7 @@ class Test(utils.TestCase):
                     [1, 2, 0, 1, 2])
         self.sample('7_4_hamming', .1, decoders,
                     [1, 0, 0, 1, 1, 0, 0],
-                    [2, 2, 2, 1, 1, 0, 0])
+                    [2, 0, 2, 1, 1, 0, 2])
 
 
 if __name__ == "__main__":
