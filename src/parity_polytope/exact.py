edce97f889754511530c218ea4a1d0829743aafd
Tharindu Adikari <5517829+thadikari@users.noreply.github.com>
2020-09-12 00:37:38 -0400

    refactoring exact.py to not call dlopen by default

--- src/parity_polytope/exact.py
+++ src/parity_polytope/exact.py
@@ -1,16 +1,29 @@
 from numpy.ctypeslib import ndpointer
+from functools import wraps
 import numpy as np
 import ctypes
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 lib_path = os.path.join(dir_path, 'ppolytope.lib')
-try:
-    lib = ctypes.cdll.LoadLibrary(lib_path)
-except OSError as err:
-    print('Caught OSError! Make sure a Python-32bit version is running.')
-    print('https://stackoverflow.com/questions/19849077/error-loading-dll-in-python-not-a-valid-win32-application.')
-    raise
+global lib
+lib = None
+
+def init_lib():
+    try:
+        global lib
+        lib = ctypes.cdll.LoadLibrary(lib_path)
+
+        ndp_int = ndpointer(ctypes.c_int, flags="C_CONTIGUOUS")
+        ndp_dbl = ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")
+
+        lib.proj_vec.argtypes = [ctypes.c_size_t, ndp_dbl, ndp_dbl]
+        lib.proj_csr.argtypes = [ctypes.c_size_t, ndp_int, ndp_dbl, ndp_dbl]
+
+    except OSError as err:
+        print('Caught OSError! Make sure a Python-32bit version is running.')
+        print('https://stackoverflow.com/questions/19849077/error-loading-dll-in-python-not-a-valid-win32-application.')
+        raise
 
 
 # https://stackoverflow.com/questions/145270/calling-c-c-from-python
@@ -18,33 +31,32 @@ except OSError as err:
 # https://stackoverflow.com/questions/5081875/ctypes-beginner
 
 
-ndp_int = ndpointer(ctypes.c_int, flags="C_CONTIGUOUS")
-ndp_dbl = ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")
+def require_init(func):
+    @wraps(func)
+    def func_wrapper(*args, **kwargs):
+        if lib is None: init_lib()
+        return func(*args, **kwargs)
+    return func_wrapper
 
-fun_proj_vec = lib.proj_vec
-fun_proj_vec.argtypes = [ctypes.c_size_t, ndp_dbl, ndp_dbl]
-
-
+@require_init
 def proj_vec(arr_in, arr_out=None):
     arr_out = np.zeros_like(arr_in, dtype=arr_in.dtype) if arr_out is None else arr_out
-    fun_proj_vec(arr_in.size, arr_in, arr_out)
+    lib.proj_vec(arr_in.size, arr_in, arr_out)
     return arr_out
 
 
-fun_proj_csr = lib.proj_csr
-fun_proj_csr.argtypes = [ctypes.c_size_t, ndp_int, ndp_dbl, ndp_dbl]
-
-
+@require_init
 def proj_csr(csr_in):
     arr_out = np.zeros_like(csr_in.data, dtype=csr_in.data.dtype)
-    fun_proj_csr(csr_in.indptr.size, csr_in.indptr, csr_in.data, arr_out)
+    lib.proj_csr(csr_in.indptr.size, csr_in.indptr, csr_in.data, arr_out)
     return arr_out
 
 
+@require_init
 def proj_rows(mat):
     arr_out = np.zeros(mat.size, dtype=mat.dtype)
     indptr = np.arange(mat.shape[0] + 1) * mat.shape[1]
-    fun_proj_csr(indptr.size, indptr, mat[:], arr_out)
+    lib.proj_csr(indptr.size, indptr, mat[:], arr_out)
     return arr_out.reshape(mat.shape)
 
 
